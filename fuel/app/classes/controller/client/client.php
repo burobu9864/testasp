3c5f55337c1ceed12c82e7c60da8f66e271e156f
you <131653120+burobu9864@users.noreply.github.com>
2024-02-02 17:14:41 +0900

    initial

--- fuel/app/classes/controller/client/client.php
+++ fuel/app/classes/controller/client/client.php
@@ -1,10 +1,19 @@
-
 <?php
-class Controller_client extends Controller
-{
-    function action_index()
-    {
-        Response::redirect('client/index.html');
-    }
 
+class Controller_Client extends Controller
+{
+    public function action_index()
+    {
+        // 何かしらのデータをビューに渡す場合
+        $data = array(
+            'name' => 'John Doe',
+            'age' => 25,
+        );
+
+        // ビューを読み込み、データを渡す
+        $view = View::forge('client/index', $data);
+
+        // レスポンスとしてビューを返す
+        return $view;
+    }
 }
