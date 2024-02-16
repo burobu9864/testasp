a8239d34d8d4ad851a45cfd1b9a817f9dfbd3661
you <131653120+burobu9864@users.noreply.github.com>
2024-02-16 16:05:43 +0900

    initial

--- fuel/app/views/advertiser/index.php
+++ fuel/app/views/advertiser/index.php
@@ -8,11 +8,22 @@
         <li class="nav-item"><a class="nav-link" href="#tab3">成果認証</a></li>
         <li class="nav-item"><a class="nav-link" href="#tab4">メディア提携</a></li>
         <li class="nav-item"><a class="nav-link" href="#tab5">原稿</a></li>
-        <li class="nav-item"><a class="nav-link" href="#tab6" <?php //echo Uri::create('advertiser/regist'); ?>>登録情報</a>
-        </li>
+        <li class="nav-item"><a class="nav-link" href="#tab6" <?php //echo Uri::create('advertiser/regist'); ?>>登録情報</a></li>
     </ul>
     <div class="tab-content">
-        <?php echo View::forge('advertiser/home/index.php'); ?>
+        <div id="tab1" class="tab-pane active">
+            <?php echo View::forge('advertiser/home/index'); ?>
+        </div>
+        <div id="tab2" class="tab-pane fade"></div>
+            <?php echo View::forge('advertiser/report/index', ['account' => $account]); ?>
+        <div id="tab3" class="tab-pane fade"></div>
+        <div id="tab4" class="tab-pane fade"></div>
+        <div id="tab5" class="tab-pane fade"></div>
+        <div id="tab6" class="tab-pane fade">
+            <?php echo View::forge('advertiser/regist/index', ['account' => $account]); ?>
+        </div>
+    </div>
+
     </div>
     <?php echo View::forge('advertiser/footer'); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -31,7 +42,7 @@
             // クリックされたタブのhref属性から対応するタブコンテンツを取得
             var target = $(this).attr('href');
             // 対応するタブコンテンツにactiveクラスを追加し、fadeクラスを削除
-            $(target).addClass('active').removeClass('fade').addClass('show');
+            $(target).addClass('active show').removeClass('fade');
             // イベントのデフォルトの動作を停止する
             return false;
         });
--- fuel/app/views/advertiser/regist/index.php
+++ fuel/app/views/advertiser/regist/index.php
@@ -1,62 +1,13 @@
-<div class="tab-pane fade" id="tab6">
-            <?php echo View::forge('advertiser/regist'); ?>
-            <div class="row-fluid">
-                <?php echo Form::label('※会社名'); ?>
-
-                <?php echo Form::input('CNAME1', '株式会社メディアグローバルステージ'); ?>
-                <div class="control-group">
-                    <label class="control-label" for="form_mail">メールアドレス</label>
-                    <div class="controls">
-                        <input class="span4" required="required" name="mail" value="OCTOPUS283@arush.jp" type="text"
-                            id="form_mail">
-                    </div>
-                </div>
-                <!-- <label for="cname">※会社名</label><input type="text" id="cname" name="CNAME1" value="株式会社メディアグローバルステージ">
-                <label for="pname">担当者名</label><input type="text" id="pname" name="PNAME1">
-                <label for="zip">※郵便番号</label><input type="text" id="zip" name="ZIPNO"><br> -->
-                <!-- <span class="f11">* 例）112-3333</span> -->
-            </div>
-            <table width="75%" border="1" cellspacing="0" cellpadding="0">
-                <tr>
-                    <td>クライアントコード</td>
-                    <td>
-                        <?php echo $account->id; ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>※メールアドレス</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->mail; ?>"></td>
-                </tr>
-                <tr>
-                    <td>※会社名</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->name; ?>"></td>
-                </tr>
-                <tr>
-                    <td>担当者名</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->staff; ?>"></td>
-                </tr>
-                <tr>
-                    <td>郵便番号</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->post_code; ?>">*
-                        例）112-3333</span></td>
-                </tr>
-                <tr>
-                    <td>住所</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->address; ?>">*
-                        例）東京都千代田区1-1-1</span></td>
-                </tr>
-                <tr>
-                    <td>電話番号</td>
-                    <td><input type="text" name="example_input" value="<?php echo $account->tel; ?>">*
-                        例）03-1111-1111</span></td>
-                </tr>
-                <tr>
-                    <td>FAX番号</td>s
-                    <td><input type="text" name="example_input" value="<?php echo $account->fax; ?>">*
-                        例）03-1111-1111</span></td>
-                </tr>
-            </table>
-            <div class="form-actions">
-                <button class="btn btn-primary" name="submit">保存</button>
-            </div>
-    </div>
+<table width="75%" border="1" cellspacing="0" cellpadding="0">
+    <tr><td>クライアントコード</td><td><?php echo $account->id; ?></td></tr>
+    <tr><td>※メールアドレス</td><td><input type="text" name="example_input" value="<?php echo $account->mail; ?>"></td></tr>
+    <tr><td>※会社名</td><td><input type="text" name="example_input" value="<?php echo $account->name; ?>"></td></tr>
+    <tr><td>担当者名</td><td><input type="text" name="example_input" value="<?php echo $account->staff; ?>"></td></tr>
+    <tr><td>郵便番号</td><td><input type="text" name="example_input" value="<?php echo $account->post_code; ?>">* 例）112-3333</span></td></tr>
+    <tr><td>住所</td><td><input type="text" name="example_input" value="<?php echo $account->address; ?>">* 例）東京都千代田区1-1-1</span></td></tr>
+    <tr><td>電話番号</td><td><input type="text" name="example_input" value="<?php echo $account->tel; ?>">* 例）03-1111-1111</span></td></tr>
+    <tr><td>FAX番号</td><td><input type="text" name="example_input" value="<?php echo $account->fax; ?>">* 例）03-1111-1111</span></td></tr>
+</table>
+<div class="form-actions">
+    <button class="btn btn-primary" name="submit">保存</button>
+</div>
