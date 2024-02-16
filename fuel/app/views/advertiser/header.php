bb7b1eaf40beb10fde3d42373325b722d0c27f6d
you <131653120+burobu9864@users.noreply.github.com>
2024-02-16 16:33:39 +0900

    initial

--- fuel/app/views/advertiser/header.php
+++ fuel/app/views/advertiser/header.php
@@ -17,6 +17,10 @@
     <link rel="icon" sizes="192x192" href="/img/touch-icon-192x192.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <?php echo Asset::js('advertiser/advertiser.js'); ?>
     <?php echo Asset::css('service/style.css'); ?>
     <?php echo Asset::css('service/form.css'); ?>
 </head>
--- fuel/app/views/advertiser/index.php
+++ fuel/app/views/advertiser/index.php
@@ -8,7 +8,7 @@
         <li class="nav-item"><a class="nav-link" href="#tab3">成果認証</a></li>
         <li class="nav-item"><a class="nav-link" href="#tab4">メディア提携</a></li>
         <li class="nav-item"><a class="nav-link" href="#tab5">原稿</a></li>
-        <li class="nav-item"><a class="nav-link" href="#tab6" <?php //echo Uri::create('advertiser/regist'); ?>>登録情報</a></li>
+        <li class="nav-item"><a class="nav-link" href="#tab6">登録情報</a></li>
     </ul>
     <div class="tab-content">
         <div id="tab1" class="tab-pane active">
@@ -23,28 +23,8 @@
             <?php echo View::forge('advertiser/regist/index', ['account' => $account]); ?>
         </div>
     </div>
-
-    </div>
-    <?php echo View::forge('advertiser/footer'); ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="/js/service/login.js"></script>
 </body>
 
+<?php echo View::forge('advertiser/footer'); ?>
+
 </html>
-<script>
-    $(document).ready(function () {
-        // タブがクリックされたときの処理
-        $('.nav-link').on('click', function () {
-            // すべてのタブコンテンツからactive, showクラスを削除し、fadeクラスを追加
-            $('.tab-pane').removeClass('active show').addClass('fade');
-            // クリックされたタブのhref属性から対応するタブコンテンツを取得
-            var target = $(this).attr('href');
-            // 対応するタブコンテンツにactiveクラスを追加し、fadeクラスを削除
-            $(target).addClass('active show').removeClass('fade');
-            // イベントのデフォルトの動作を停止する
-            return false;
-        });
-    });
-</script>
